86e397396d97898064896adf1743625cbe0dcd16
y33988979 <33988979@163.com>
2017-04-25 17:20:51 +0800

    create mcu communicate code

---
+++ include/dnq_mcu.h
@@ -0,0 +1,18 @@
+#ifndef _DNQ_MCU_H_
+#define _DNQ_MCU_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+#include <typedef.h>
+
+
+        
+
+#endif /* _DNQ_MCU_H_ */
+
---
+++ src/dnq_mcu.c
@@ -0,0 +1,32 @@
+/* 
+ * cpu and mcu communicate Program
+ * 
+ *  Copyright (c) 2017 yuchen
+ *  Copyright 2017-2017 jiuzhoutech Inc.
+ *  yuchen  <yuchen@jiuzhoutech.com, 382347665@qq.com>
+ * 
+ *  this is a communicate Program between cpu and mcu.
+ *  mcu1:  heater control      protocol: RS232
+ *  mcu2:  temperature sensor  protocol: RS485
+ *
+ * Note : 
+ */
+
+
+#include "dnq_common.h"
+#include "ngx_palloc.h"
+#include "dnq_mcu.h"
+
+
+S32 dnq_mcu_init()
+{
+    
+    return 0;
+}
+
+S32 dnq_mcu_deinit()
+{
+    
+    return 0;
+}
+
