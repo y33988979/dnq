bc1f7ac73cab9037fb65af99e38f9794f3f5b0ac
y33988979 <ychen>
2017-10-21 20:26:35 +0800

    1. update init.sh, rm exit.dump if it is too large. 2. add upgrd_done.sh script. 3. modify upgrade package maker.

--- bin/init.sh
+++ bin/init.sh
@@ -4,5 +4,11 @@ if [ -f /root/dnq/SN.conf ];then
     insmod /root/dnq/w1_ds18b20.ko
 fi
 
+size=`stat -c %s exit.dump`
+if [ $size -gt 102400 ];then
+    rm -rf exit.dump
+    echo "delete exit.dump"
+fi
+
 echo "dnqV2 ready!"
 
---
+++ bin/script/upgrd_done.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -rf /root/dnq/exit.dump
+rm -rf /root/dnq/dnq.dat
+
+echo "cleanup necessary data for upgrade!"
+
