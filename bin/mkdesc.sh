559b4ea6739786c2b0142638e3d9c1190b94efd6
y33988979 <ychen>
2018-01-28 13:58:02 +0800

    修复升级描述符制作工具

--- bin/mkdesc.sh
+++ bin/mkdesc.sh
@@ -11,8 +11,8 @@ file_type="01"
 mode="01"
 need_ver="0000"
 mac="70b3d5cf4924"
-hw_ver=0100
-sw_ver=0101
+hw_ver=0`echo $HardwareVersion |cut -c 3-6`
+sw_ver=0`echo $SoftwareVersion |cut -c 3-6`
 crc_32=`crc32 $input_file | tr [a-z] [A-Z]`
 
 echo $tag$upgrade_type$file_type$mode$need_ver$mac$hw_ver$sw_ver$crc_32 > $output_file
